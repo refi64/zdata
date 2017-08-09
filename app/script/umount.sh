9b22ca59c298fa153848fd84fc12e72adbc1446c
Ryan Gonzalez <rymg19@gmail.com>
2017-08-09 14:50:39 -0500

    Fixes

--- app/script/umount.sh
+++ app/script/umount.sh
@@ -5,9 +5,10 @@ set -ex
 [ -n "$1" ] || exit 1
 cd /data/data/com.refi64.zdata.app/app_flutter
 
+pm clear "$1" ||:
 chmod +x fusermount
 ./fusermount -u "/data/data/$1" ||:
 pm clear "$1" ||:
-rm -rf "storage/$1"
+rm -rf "storage/$1" ||:
 
 svc power reboot ||:
