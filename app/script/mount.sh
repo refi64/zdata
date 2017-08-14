e7c361194ec69095d07dd64ffad7150e3aa7cfd5
Ryan Gonzalez <rymg19@gmail.com>
2017-08-14 12:49:51 -0500

    Fix a bug where data is cleared on reboot

--- app/script/mount.sh
+++ app/script/mount.sh
@@ -7,7 +7,6 @@ cd /data/data/com.refi64.zdata.app/app_flutter
 
 user=`stat -c '%U' "/data/data/$1" | tr -d ' '`
 pm clear "$1"
-rm -rf "storage/$1"
 mkdir -p "storage/$1"
 chmod +x fusecompress
 ./fusecompress -o allow_other,nonempty,fc_c:zlib "storage/$1" "/data/data/$1"
